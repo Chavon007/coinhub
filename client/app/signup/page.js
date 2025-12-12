7a1e4d226555ce154df1785663ba46d0793bae03
azuh salvation <salvationazuh@gmail.com>
2025-12-12 09:59:47 +0100

    update

--- client/app/signup/page.js
+++ client/app/signup/page.js
@@ -1,4 +1,5 @@
 "use client";
+import { FaBitcoin } from "react-icons/fa6";
 import { useState } from "react";
 import { useAuth } from "@/context/authContext";
 
@@ -38,8 +39,15 @@ function Signup() {
     ) {
       setError("Please all required fields");
       return;
-    } else if (!/^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/.test(formData.email)) {
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
       setError("Use a proper email address");
+      return;
+    } else if (
+      !/^(?=.*[A-Z])(?=.*[!@#$%^&*])(?=.{8,})/.test(formData.password)
+    ) {
+      setError(
+        "Password must be at least 8 characters long, contain one uppercase letter, and one special symbol"
+      );
       return;
     } else if (formData.password !== formData.confirmPassword) {
       setError("Password and confirm password does not match");
@@ -60,19 +68,34 @@ function Signup() {
     }
   };
   return (
-    <div className="container mx-auto bg-background h-auto">
-      <div>
-        <div>
-          <h1>Coinhub</h1>
+    <div className="container mx-auto bg-background h-auto md:h-[100vh] lg:h-auto pb-[20px] w-full">
+      <div className="w-[90%] mx-auto h-auto flex flex-col gap-2">
+        <div className="w-[20%] p-[10px] mt-[10px]">
+          <h1 className="font-orbitron text-3xl bold text-text-secondary">
+            Coinhub
+          </h1>
         </div>
 
-        <div>
-          <h3>Create an account to get Started</h3>
-          <form onSubmit={handleSubmit}>
-            {error && <p>{error}</p>}
-            {success && <p>{success}</p>}
-            <label htmlFor="firstname">First Name:</label>
+        <div className="flex flex-col justify-center h-auto">
+          <h3 className="lg:w-[50%] mx-auto flex items-center justify-center gap-3 text-center lg:max-w-[500p] font-outfit text-text-secondary  text-base lg:text-2xl font-bold p-[10px]">
+            <span className="text-4xl">
+              {" "}
+              <FaBitcoin />
+            </span>
+            <span>Create an account to get started</span>
+          </h3>
+          <form
+            className="w-[100%] lg:w-[60%] mx-auto py-[30px] flex flex-col bg-[#121826] p-[10px] gap-3 border border-[#1F2937] border-3"
+            onSubmit={handleSubmit}
+          >
+            <label
+              htmlFor="firstname"
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+            >
+              First Name:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="text"
               value={formData.firstName}
               placeholder="Enter first name"
@@ -80,8 +103,14 @@ function Signup() {
                 setFormData({ ...formData, firstName: e.target.value })
               }
             />
-            <label htmlFor="lastname">Last Name:</label>
+            <label
+              htmlFor="lastname"
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+            >
+              Last Name:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="text"
               value={formData.lastName}
               placeholder="Enter last name"
@@ -89,8 +118,14 @@ function Signup() {
                 setFormData({ ...formData, lastName: e.target.value })
               }
             />
-            <label htmlFor="email">Email:</label>
+            <label
+              htmlFor="email"
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+            >
+              Email:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="email"
               value={formData.email}
               placeholder="Enter email"
@@ -98,8 +133,14 @@ function Signup() {
                 setFormData({ ...formData, email: e.target.value })
               }
             />
-            <label htmlFor="password">Pasword:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="password"
+            >
+              Pasword:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="password"
               value={formData.password}
               placeholder="Enter Password"
@@ -107,8 +148,14 @@ function Signup() {
                 setFormData({ ...formData, password: e.target.value })
               }
             />
-            <label htmlFor="password">Confirm Pasword:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="password"
+            >
+              Confirm Pasword:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="password"
               value={formData.confirmPassword}
               placeholder="Confirm Password"
@@ -116,8 +163,14 @@ function Signup() {
                 setFormData({ ...formData, confirmPassword: e.target.value })
               }
             />
-            <label htmlFor="city">City:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="city"
+            >
+              City:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="text"
               value={formData.city}
               placeholder="Enter your city"
@@ -125,8 +178,14 @@ function Signup() {
                 setFormData({ ...formData, city: e.target.value })
               }
             />
-            <label htmlFor="country">Country:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="country"
+            >
+              Country:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="text"
               value={formData.country}
               placeholder="Enter your country"
@@ -134,8 +193,14 @@ function Signup() {
                 setFormData({ ...formData, country: e.target.value })
               }
             />
-            <label htmlFor="YOB">Year of Birth:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="YOB"
+            >
+              Year of Birth:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="number"
               value={formData.YOB}
               placeholder="Enter year of birth"
@@ -143,8 +208,14 @@ function Signup() {
                 setFormData({ ...formData, YOB: e.target.value })
               }
             />
-            <label htmlFor="NIN">Your NIN:</label>
+            <label
+              className="font-nunito-sans text-[#9CA3AF] text-base font-semibold"
+              htmlFor="NIN"
+            >
+              Your NIN:
+            </label>
             <input
+              className="bg-[#0B0F1A] p-[10px] rounded rounded-1xl placeholder:text-[#E5E7EB] text-xs font-outfit italic focus:outline-none text-[#9CA3AF]"
               type="number"
               value={formData.nin}
               placeholder="Enter your NIN"
@@ -152,6 +223,19 @@ function Signup() {
                 setFormData({ ...formData, nin: e.target.value })
               }
             />
+
+            <button
+              className="w-[50%] mx-auto max-w-[200px] rounded rounded-1xl p-[8px] text-[#E5E7EB] font-outfit text-sm bg-[#00D395]"
+              type="submit"
+            >
+              {loading ? "Creating account" : "Create Acoount"}
+            </button>
+            {error && (
+              <p className="text-[#F87171] text-xs font-outfit">{error}</p>
+            )}
+            {success && (
+              <p className="text-[#22C55E] text-xs font-outfit">{success}</p>
+            )}
           </form>
         </div>
       </div>
