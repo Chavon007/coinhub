0dff4c785ef042a11997376596eee9cd7b2a1afa
azuh salvation <salvationazuh@gmail.com>
2025-12-15 17:41:50 +0100

    update

--- client/app/connectwallet/page.js
+++ client/app/connectwallet/page.js
@@ -1,7 +1,63 @@
+import Link from "next/link";
+import Image from "next/image";
+import { FaArrowLeft } from "react-icons/fa";
 function Connectwallet() {
   return (
     <div>
-      <div>here</div>
+      <div>
+        <div>
+          <Link href="/choosewallet">
+            <span>
+              <FaArrowLeft />
+            </span>
+            <span>Back</span>
+          </Link>
+        </div>
+        <div>
+          <div>
+            <h3>Connect Wallet</h3>
+            <p>Choose your preferred wallet provider</p>
+          </div>
+
+          <div>
+            <Link href="">
+              <div>
+                <Image
+                  src="/metamask.png"
+                  alt="metamask"
+                  width={100}
+                  height={100}
+                />
+              </div>
+
+              <div>
+                <h6>MetaMask</h6>
+                <p>Connect to MetaMask wallet</p>
+              </div>
+            </Link>
+            <Link href="">
+              <div>
+                <Image
+                  src="/walletconnect.png"
+                  alt="walletconnect"
+                  width={100}
+                  height={100}
+                />
+              </div>
+
+              <div>
+                <h6>WalletConnect</h6>
+                <p>Scan QR to connect Your wallet</p>
+              </div>
+            </Link>
+          </div>
+
+          <div>
+            <p>Don't have a wallet?</p>
+            <Link href="">Create one now</Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
