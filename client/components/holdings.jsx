e1e310f649bd2769444c39a9979c22892b57cfb9
azuh salvation <salvationazuh@gmail.com>
2026-01-14 14:46:33 +0100

    update

--- client/components/holdings.jsx
+++ client/components/holdings.jsx
@@ -65,22 +65,24 @@ function Holdings() {
       </p>
     );
   return (
-    <div className="w-[98%] flex justify-between items-center h-auto">
-      <div className="bg-surface w-[75%]   p-[10px]">
+    <div className="w-[98%] flex justify-between  gap-3 items-center h-auto">
+      <div className="bg-surface w-[70%]  rounded-2xl p-[10px]">
         <h4 className="text-text-primary ml-[20px] font-nunito-sans text-1xl font-semibold">
           Holdings
         </h4>
 
-        <table className="p-2 w-full border-collapse rounded-[10px]">
+        <table className="p-2 w-full border-collapse">
           <thead className=" bg-surface border-b border-gray-200 ">
             <tr className="text-left text-sm text-text-secondary">
-              <th className="py-3 px-4  font-outfit font-bold">Asset</th>
-              <th className="py-3 px-4 font-semibold">Amount</th>
-              <th className="py-3 px-4 font-semibold">Price</th>
-              <th className="py-3 px-4 font-semibold">Value</th>
-              <th className="py-3 px-4 font-semibold">Invested</th>
-              <th className="py-3 px-4 font-semibold">P&amp;L</th>
-              <th className="py-3 px-4 font-semibold">P&amp;L %</th>
+              <th className="py-3 px-4  text-center font-outfit font-bold">
+                Asset
+              </th>
+              <th className="py-3 px-4 text-center font-semibold">Amount</th>
+              <th className="py-3 px-4 font-semibold text-center">Price</th>
+              <th className="py-3 px-4 font-semibold text-center">Value</th>
+              <th className="py-3 px-4 font-semibold text-center">Invested</th>
+              <th className="py-3 px-4 font-semibold text-center">P&amp;L</th>
+              <th className="py-3 px-4 text-center font-semibold">P&amp;L %</th>
             </tr>
           </thead>
 
@@ -99,7 +101,9 @@ function Holdings() {
                   </span>
                   <span className="flex flex-col">
                     <span>{h.abb}</span>
-                    <span className="text-xs text-text-secondary">{h.asset}</span>
+                    <span className="text-xs text-text-secondary">
+                      {h.asset}
+                    </span>
                   </span>
                 </td>
                 <td className="p-6 font-nunito-sans text-text-primary font-medium text-sm">
@@ -136,18 +140,41 @@ function Holdings() {
         </table>
       </div>
 
-      <div>
-        <h2>Token Allocation</h2>
+      <div className="bg-surface w-[40%] rounded-2xl h-[350px] p-[10px]">
+        <h2 className="text-text-primary py-2 font-nunito-sans text-1xl font-bold">
+          Token Allocation
+        </h2>
 
         {theHolding.map((t, index) => (
           <div key={index}>
-            <p>
-              <span>{t.asset}</span> <span>{t.pnlPercentage}</span>
+            <p className="flex p-4 justify-between items-center">
+              <span className="flex items-center gap-3">
+                <span>
+                  <Image
+                    src={t.image}
+                    alt={t.abb}
+                    width={20}
+                    height={20}
+                    className="rounded-2xl"
+                  />
+                </span>
+                <span className="font-nunito-sans text-text-primary font-bold text-1xl">
+                  {t.abb}
+                </span>
+              </span>{" "}
+              <span className="text-text-secondary text-1xl font-outfit ">
+                {t.pnlPercentage}%
+              </span>
             </p>
-            <p
-              className="text-accent-green h-2 rounded-full transition-all"
-              style={{ width: `${t.pnlPercentage}%` }}
-            ></p>
+
+            <div className="w-full bg-background  h-2 rounded-full">
+              <div
+                className={`h-2 rounded-full transition-all ${
+                  t.pnlPercentage >= 0 ? "bg-accent-green" : "bg-red-500"
+                }`}
+                style={{ width: `${Math.abs(t.pnlPercentage)}%` }}
+              ></div>
+            </div>
           </div>
         ))}
       </div>
